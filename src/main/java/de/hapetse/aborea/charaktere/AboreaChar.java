2d72d7885a396d2f8ca9ebd8d3557072ea01f06c
Hansel <hansel@mailbox.org>
2022-04-11 13:43:25 +0200

    Charaktere hinzugefügt

---
+++ src/main/java/de/hapetse/aborea/charaktere/AboreaChar.java
@@ -0,0 +1,33 @@
+package de.hapetse.aborea.charaktere;
+
+abstract class AboreaChar{
+    private String name;
+
+    private String beruf;
+    private String rasse;
+
+    private int stärke;
+    private int stärkeBonus;
+    private int geschicklichkeit;
+    private int geschicklichkeitsBonus;
+    private int konsitutionsBonus;
+    private int konstitution;
+    private int intelligenz;
+    private int intelligenzBonus;
+    private int charisma;
+    private int charismaBonus;
+
+    private int tp;
+    private int rüstungsklasse;
+    private int mp;
+    private int ap;
+
+    private int athletik;
+    private int einflussnahme;
+    private int gezielteEinflussnahme;
+    private int kunst;
+    private int list;
+    private int magieEntwickeln;
+    private int natur;
+
+}
